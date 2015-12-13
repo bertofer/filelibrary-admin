97b2e248ce38eae1d91f5dfab09c625b656def06
Alberto Fernandez <alberto.fernandez.491@gmail.com>
2015-12-13 18:52:42 +0000

    Updated example rundocker

--- docker/rundocker.sh
+++ docker/rundocker.sh
@@ -1,19 +1,29 @@
 #/bin/sh
 # This is an example file. Copy it to your folder and fill the env variables of
 # your environment.
-export APP_PORT=
 
-docker run -p 80:$APP_PORT \
+export NAME=filelibrary-admin
+export APP_PORT=
+export SERVER_ADDRESS=""
+export FILES_FOLDER_INSIDE_ADMIN=""
+export FILES_FOLDER_OUTSIDE_ADMIN=""
+
+docker stop $NAME
+docker rm $NAME
+
+docker run -d  \
 --expose $APP_PORT \
--e SERVER_PROTOCOL='http | https' \
--e SERVER_ADDRESS="" \
+-v $FILES_FOLDER_INSIDE_ADMIN:$FILES_FOLDER_OUTSIDE_ADMIN:rw \ #We share the folder to serve files directly with nginx
+-e SERVER_PROTOCOL='' \
+-e SERVER_ADDRESS=$SERVER_ADDRESS \
 -e APP_PORT=$APP_PORT \
 -e TEMP_DIR="" \
--e FINAL_DIR="" \
+-e FINAL_DIR=$FILES_FOLDER \
 -e AUTHOR="" \
 -e WEBSEED_ADDRESS="" \
 -e WEBSEED_PORT=324 \
 -e TRACKER_ADDRESS="" \
 -e TRACKER_PORT=423 \
 -e DB_PATH="" \
+--name $NAME \
 albertoferbcn/filelibrary-admin
